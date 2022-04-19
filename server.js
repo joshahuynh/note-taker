6870c6eead56a66f786547a3a0698844c4a9cb9b
joshahuynh <joshahuynh@gmail.com>
2022-04-18 22:34:25 -0500

    working notes

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const path = require('path');
 const express = require('express');
 const uniqid = require('uniqid');
 let noteDB = require('./db/db.json');
+
+// instantiate the server
 const app = express();
+
 // set port
 const PORT = process.env.PORT || 3000;
 
@@ -19,20 +22,27 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.post('/api/notes', (req, res) => {
-    req.body.id = uniqid();
-    const newNote = createNewNote(req.body, noteDB)
-    res.json(req.body)
+    // add an unique id to new note
+    req.body.id=uniqid();
+    const newNote= createNewNote(req.body,noteDB)
+    res.json(noteDB)
 });
 
+// function to push new note and add to note database
 function createNewNote(body, noteArray){
     const newNote = body;
-    noteArray.push(newNote)
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({noteDB: noteArray}, null, 2)
-    );
+    noteArray.push(newNote) 
+    fs.writeFileSync('./db/db.json',JSON.stringify(noteDB))
     return newNote
 };
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, ("public/notes.html")));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, ("./public/index.html")));
+});
 
 app.listen(PORT, () => {
     console.log(`API now on port ${PORT}!`)
