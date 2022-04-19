c568cdbebcf27acd5572a7af314f2737ca7ed889
joshahuynh <joshahuynh@gmail.com>
2022-04-18 19:27:31 -0500

    heroku create and port

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 let noteDB = require('./db/db.json')
+const PORT = process.env.PORT || 3000;
 
 app.get('/api/notes', (req, res) => {
     res.json(noteDB)
 })
 
-app.listen(3000, () => {
-    console.log('API on port 3000!')
+app.listen(PORT, () => {
+    console.log(`API now on port ${PORT}!`)
 })
